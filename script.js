1067a797f4c6b4a7ab934ca94b856e3442cc2478
DIPTENDU MANDAL <118509862+diptendu13@users.noreply.github.com>
2023-06-14 10:58:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-function threeSum(arr, target) {
+function threeSum() {
 // write your code here
-  
+	let deci = Number(document.getElementById("decimal").value);
+	let binary = "";
+	while (deci > 0){
+		if (deci % 2 == 1) {
+			binary = '1' + binary;
+		}
+		else{
+			binary = '0' + binary;
+		}
+		deci = Math.floor(deci / 2);
+	}
 }
 
 module.exports = threeSum;
