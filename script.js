3e2756a8569f1e0378fc39c7fd2e6995a86cd68a
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-07-07 15:15:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-function threeSum() {
-// write your code here
-	let deci = Number(document.getElementById("decimal").value);
-	let binary = "";
-	while (deci > 0){
-		if (deci % 2 == 1) {
-			binary = '1' + binary;
-		}
-		else{
-			binary = '0' + binary;
-		}
-		deci = Math.floor(deci / 2);
-	}
-	document.getElementById("ans").innerHTML = "The binary value is " + binary;
+function decimalToBinary(num) {
+  // Write your code here
 }
+
+window.decimalToBinary = decimalToBinary;
